d0c24cd674933e1b8bd7f7d2b967c0a74d502234
RingSho <s.0v0xx@outlook.jp>
2020-04-25 10:15:58 +0900

    Add:some code and test

--- src/image_translator.py
+++ src/image_translator.py
@@ -2,7 +2,6 @@
 import os
 
 import cv2
-import numpy as np
 
 
 class ImageTranslator:
@@ -11,13 +10,22 @@ class ImageTranslator:
         self.image_path = image_path
         self.image_name = os.path.splitext(os.path.basename(image_path))[0]
         self.image = cv2.imread(image_path)
-        
+
     def get_image_shape(self) -> tuple:
-        """画像の高さと幅をタプルで返す"""
+        """画像の高さと幅をタプル(height, width)で返す"""
         self.height, self.width = self.image.shape[:2]
         return (self.height, self.width)
     
-    def resize_half(self):
+    def get_image_name(self) -> str:
+        """ファイル名を返す"""
+        return str(self.image_name)
+    
+    def get_image_ext(self) -> str:
+        """拡張子を返す"""
+        root, ext = os.path.splitext(self.image_path)
+        return str(ext)
+
+    def resize2half(self):
         """1/2に画像をリサイズして返す"""
         self.image = cv2.resize(self.image, dsize=None, fx=0.5, fy=0.5)
         return self.image
@@ -28,17 +36,20 @@ class ImageTranslator:
         return self.image
     
     def resize2target_size(self, height, width):
+        """指定したサイズにリサイズした画像を返す"""
         self.image = cv2.resize(self.image, dsize=(width, height))
         return self.image
 
 
 if __name__ == "__main__":
     image_translator = ImageTranslator("images/octocat.png")
-    print(image_translator.get_image_shape())
+    print("image name: ", image_translator.get_image_name())
+    print("image size: ", image_translator.get_image_shape())
+    print("image ext: ", image_translator.get_image_ext())
 
-    image_translator.resize_half()
-    print(image_translator.get_image_shape())
+    image_translator.resize2half()
+    print("image size: ", image_translator.get_image_shape())
 
     target_h, target_w = (256, 196)
     image_translator.resize2target_size(target_h, target_w)
-    print(image_translator.get_image_shape())
+    print("image size: ", image_translator.get_image_shape())
--- test/test_image_translator.py
+++ test/test_image_translator.py
@@ -1,5 +1,36 @@
 #! python3
-import image_translator
 import pytest
 
+from src.image_translator import ImageTranslator
 
+
+def test_get_image_name():
+    image_translator = ImageTranslator("images/octocat.png")
+    assert "octocat" == image_translator.get_image_name()
+
+def test_get_image_ext():
+    image_translator = ImageTranslator("images/octocat.png")
+    assert ".png" == image_translator.get_image_ext()
+
+def test_get_image_shape():
+    """長さが2で要素がintのtupleが返ってくる"""
+    image_translator = ImageTranslator("images/octocat.png")
+    assert tuple == type(image_translator.get_image_shape())
+    assert int == type(image_translator.get_image_shape()[0])
+    assert int == type(image_translator.get_image_shape()[1])
+    assert 2 == len(image_translator.get_image_shape())
+
+def test_resize2half():
+    image_translator = ImageTranslator("images/octocat.png")
+    image_translator.resize2half()
+    assert (1000, 1000) == image_translator.get_image_shape()
+
+def test_resize2twice():
+    image_translator = ImageTranslator("images/octocat.png")
+    image_translator.resize2twice()
+    assert (4000, 4000) == image_translator.get_image_shape()
+    
+def test_resize2target_size():
+    image_translator = ImageTranslator("images/octocat.png")
+    image_translator.resize2target_size(256, 192)
+    assert (256, 192) == image_translator.get_image_shape()
